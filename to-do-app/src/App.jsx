934b5ddd943a0bee616b9f76d813d9311c98ba38
Viktoriia Danylko <108232067+WonderfulPie@users.noreply.github.com>
2025-12-15 15:00:36 +0200

    feat: #18 Define project folder structure (Closes #18)

---
+++ to-do-app/src/App.jsx
@@ -0,0 +1,9 @@
+import styles from './index.css';
+
+export default function App() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Hello World</h1>
+    </div>
+  )
+}
